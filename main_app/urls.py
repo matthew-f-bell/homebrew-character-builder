12cd7417110dbc7d95f8d0aeb12e4a7bbb40bccf
xdjx3runx <xdjx3runx@gmail.com>
2022-04-09 22:45:15 -0700

    auth templates created

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
+    path('', views.Character_List.as_view(), name='home'),
     path('characters/', views.Character_List.as_view(), name="character-list"),
     path('characters/new/', views.Character_Create.as_view(), name="character-create"),
     path('characters/<int:pk>/', views.Character_Detail.as_view(), name="character-detail"),
