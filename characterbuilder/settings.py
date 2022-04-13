60186e29d6d480d557a1f24a0d71349e4a9f4653
xdjx3runx <xdjx3runx@gmail.com>
2022-04-12 18:36:48 -0700

    reset settings for deployment

--- characterbuilder/settings.py
+++ characterbuilder/settings.py
@@ -18,8 +18,8 @@ import dj_database_url
 # from dotenv import load_dotenv
 # load_dotenv()
 
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # If the host name starts with 'live', DJANGO_HOST = "production"
 if socket.gethostname().startswith('live'):
@@ -119,7 +119,7 @@ DATABASES = {
     }
 }
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
